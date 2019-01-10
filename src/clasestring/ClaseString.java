92a08de4a6aadb062e70113c4b2034457b575646
Naomi <zarza.naomi@gmail.com>
2019-01-10 14:07:32 +0100

    Métodos compareTo(), toUpperCase() y toLowerCase()

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -56,5 +56,25 @@ public class ClaseString {
         // Métodos equals y equalsIgnoreCase
         System.out.println("Igualdad Java y java: " + "Java".equals("java"));
         System.out.println("Igualdad Java y java: " + "Java".equalsIgnoreCase("java"));
+        
+        // Comparación de cadenas (diccionario)
+        // Devuelve cero si las cadeas son iguales, un negativo si s1 está
+        // antes de s2 en el diccionario (menor) o un positivo si s1 está
+        // después de s2 (mayor) - Lo usaremos mucho.
+        
+        int compareTo = s1.compareTo(s1);
+        //Me devuelve cero porque son iguales, estamos comparándolo consigo mismo.
+        System.out.println("Comparación de s1 y s1: " + compareTo);
+        
+        compareTo = s1.compareTo("aes");
+        System.out.println("Comparación de s1 y aes: " + compareTo);
+        
+        compareTo = "aes".compareTo(s1);
+        System.out.println("Comparación de aes y s1: " + compareTo);
+        
+        // Conversión mayúsculas y minúsculas
+        String mayusculas = s2.toUpperCase();
+        System.out.println("s2 en mayúsculas: " + mayusculas);
+        System.out.println("String mayusculas en minúsculas " + mayusculas.toLowerCase());
     }
 }
